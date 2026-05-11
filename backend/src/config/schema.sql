f2f6b0bd5cf98029f269fc7641b7d48ab6f3fd3f
Suraj6204 <suraj.2023ug4001@iiitranchi.ac.in>
2026-05-11 13:25:31 +0530

    profile schema and validators

--- backend/src/config/schema.sql
+++ backend/src/config/schema.sql
@@ -6,3 +6,18 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TYPE gender_enum AS ENUM ('male', 'female', 'other');
+
+CREATE TABLE profile(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER UNIQUE NOT NULL REFERENCS users(id) ON DELETE CASCADE,
+  bio TEXT,
+  dob Date NOT NULL,
+  gender gender_enum NOT NULL,
+  gender_preference gender_enum NOT NULL,
+  location JSONB, -- { lat: 12.34, lng: 56.78 }
+  interests TEXT[],
+  images TEXT[] DEFAULT '{}',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
---
+++ backend/src/validators/profile.validator.ts
@@ -0,0 +1,18 @@
+import {z} from 'zod'
+
+const GenderEnum = z.enum(['male', 'female', 'other']);
+
+export const profileSchema = z.object({
+    bio: z.string().max(500).optional(),
+    dob: z.string().refine((date)=> !isNaN(Date.parse(date)) , {
+        message: "Invalid date format",
+    }),
+    gender : GenderEnum,
+    gender_preference : GenderEnum,
+    location: z.object({
+        lat: z.number(),
+        lng: z.number()
+    }).optional(),
+    interests: z.array(z.string()).min(1,"Select at least one interest"),
+    images: z.array(z.url()).min(4,"Upload at least four photos"),
+});
